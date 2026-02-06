9e1f9c4c77129dae4466c3e50041bb27965135c1
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-02-06 22:51:22 +0100

    add template for job router

---
+++ routes/job.route.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const route = express.Router();
+const job = require("../models/job.model");
+
+const getAllJob = route.get("/All", async(res, req) =>{
+    try{
+        //
+    } catch{
+        //
+    }
+} );
+
+const getOneJob = route.get("/:id", async(res, req) =>{
+    try{
+        //
+    } catch{
+        //
+    }
+} );
+
+
+const getJobByType = route.get("/:id", async(res, req) =>{
+    try{
+        //
+    } catch{
+        //
+    }
+} );
+
+
+const getJobByData = route.get("/:id", async(res, req) =>{
+    try{
+        //
+    } catch{
+        //
+    }
+} );
+
+const sortJobByDatePost = route.get("/:id", async(res, req) =>{
+    try{
+        //
+    } catch{
+        //
+    }
+} );
+
+const sortJobPostDate = route.get("/:id", async(res, req) =>{
+    try{
+        //
+    } catch{
+        //
+    }
+} );
+
+
+const AddNewPost = route.post("/:id", async(res, req) =>{
+    try{
+        //
+    } catch{
+        //
+    }
+} );
+
+
+
+
+const updateJob = route.put("/:id", async(res, req) =>{
+    try{
+        //
+    } catch{
+        //
+    }
+} );
+
+
+const deleteJob = route.delete("/:id", async(res, req) =>{
+    try{
+        //
+    } catch{
+        //
+    }
+} );
+
+const UpdateAnswer = route.patch("/:id", async(res, req) =>{
+    try{
+        //
+    } catch{
+        //
+    }
+} );
+
+
