0a2bc32369399c0303b60499888394dc9083f978
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-02-09 10:35:51 +0100

    ajust the job model

--- models/job.model.js
+++ models/job.model.js
@@ -2,58 +2,37 @@ const mongoose = require ("mongoose");
 
 
 const JobSchema = mongoose.Schema({
-    title: {
+    poste: {
         type: String,
         required: true
     },
-    company:{
+    entrprise:{
         type: String,
         required: true
     },
-    requiredSkills: {
+    offerLink:{
         type: String,
         required: true
-    },
+    }
+    ,
     typeOfJob: {
         type: String,
-        enum: ["Alternance", "stage", "CDI", "CDD", "Freelance", "Temps plein", "Temps partiel", "job etudian", "Autre"],
+        enum: ["alternance", "stage", "CDI", "CDD", "Freelance", "Temps plein", "Temps partiel", "job etudian", "Autre"],
         default: "Autre"
-    },
-    description: {
-        type: String,
-        required: true
     },
     // user: {
     //     type: mongoose.Schema.Types.ObjectId,
     //     ref: 'User'
     // }
     // ,
-    date_posted: {
-        type: Date,
-        // required: true
-    },
-    date_candidature: {
-        type: Date,
-        // required: true
-    },
-    moyen_candidature:{
-        type: String
-    },
+    // date_candidature: {
+    //     type: Date,
+    //     required: true
+    // },
     status: {
         type: String,
         enum: ["En attente", "entretien prevu",  "Accepté", "Refusé"],
         default: "En attente"
-    },
-    contact: {
-        site_web: {type: String},
-        email: {type: String},
-        phone: {type: String},
-        linkedin: {type: String}
-    },
-    rappel: {
-        premier_rappel: {type: Date },
-        second_rappel: {type: Date},
-        troisieme_rappel: {type: Date}
     },
 }, {
     timestamps: {createdAt: true}
